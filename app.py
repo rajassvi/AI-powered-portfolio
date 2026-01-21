ac2d42b40e49875d7f6aab92d816ce1974641434
Rajassvi V <rajassviv@Rajassvis-MacBook-Air.local>
2026-01-22 01:16:41 +0530

    Your commit message

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ Message:
 # App Run
 # --------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5001)
